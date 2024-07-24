7eb41cc4e23986bcc7413ecbdb244c55933bfba1
Bruno henrique queiroz da silva <brunohenriquetrab@gmail.com>
2024-07-24 18:27:25 -0300

    Update endereco.sql

--- endereco.sql
+++ endereco.sql
@@ -1,10 +1,10 @@
-CREATE table clientes(
-id_clientes INT AUTO_INCREMENT,  
-nome VARCHAR(64)NOT NULL,
-email VARCHAR(64), 
-senha VARCHAR(16) NOT NULL,
-Telefone VARCHAR (11),
-data_cadastro DATE,
-data_nascimento DATE,
-PRIMARY KEY  (id_clientes)
+CREATE TABLE endereço (
+id_endereço PRIMARY KEY,
+rua VARCHAR(100) NOT NULL,
+numero VARCHAR(100) NOT NULL,
+bairro VARCHAR(100) NOT NULL,
+cidade VARCHAR(100) NOT NULL,
+estado VARCHAR(100) NOT NULL,
+cep VARCHAR(100) NOT NULL,
+complemento VARCHAR(100) NOT NULL
 );
