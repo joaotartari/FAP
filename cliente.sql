e160daf7bb1bd4af799807996849c015822c7e3b
brunohenrique66 <brunohenriquetrab@gmail.com>
2024-07-24 18:19:45 -0300

    Tabela clientes criada.

--- cliente.sql
+++ cliente.sql
@@ -1,1 +1,9 @@
-CREATE 
+CREATE table clientes(
+id_clientes INT, AUTO INCREMENT, PRIMARY KEY
+nome_clientes VARCHAR (64) NOT NULL
+email VARCHAR(64) 
+senha VARCHAR(16) NOT NULL
+Telefone VARCHAR (11)
+data_cadastro DATA
+data_nascimento DATA
+);
